e6145c826bfe1ca44aad0aee7459350a9eedee62
Alexey Golubev <alexey.golubev@onlyoffice.com>
2016-03-31 14:52:32 +0300

    Update GenerateAllFonts.sh

--- common/documentserver/server/tools/GenerateAllFonts.sh
+++ common/documentserver/server/tools/GenerateAllFonts.sh
@@ -5,8 +5,8 @@ DIR="/var/www/onlyoffice/documentserver"
 #Start generate AllFonts.js, font thumbnails and font_selection.bin
 sudo -u onlyoffice "$DIR/server/tools/AllFontsGen"\
  "/usr/share/fonts"\
- "$DIR/sdkjs/Common/AllFonts.js"\
- "$DIR/sdkjs/Common/Images"\
+ "$DIR/sdkjs/common/AllFonts.js"\
+ "$DIR/sdkjs/common/Images"\
  "$DIR/server/FileConverter/bin/font_selection.bin"
 
 #Restart web-site and converter
