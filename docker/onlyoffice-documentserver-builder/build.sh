428c329f93260541d55053aef974bbd04aef6bb6
Alexey Golubev <alexey.golubev@onlyoffice.com>
2016-09-14 16:17:54 +0300

    Update build.sh

--- docker/onlyoffice-documentserver-builder/build.sh
+++ docker/onlyoffice-documentserver-builder/build.sh
@@ -5,5 +5,5 @@ BUILD_DIR=/var/lib/onlyoffice
 cd $BUILD_DIR/sdkjs && make clean && make
 cd $BUILD_DIR/server && make clean && make all build-date htmlfileinternal
 cd $BUILD_DIR/document-server-integration/web/documentserver-example/nodejs && npm install
-cd $BUILD_DIR/core/Common/3dParty && ./build.sh
+cd $BUILD_DIR/core/Common/3dParty && ./make.sh
 cd $BUILD_DIR/package && make -e clean && make -e deb
