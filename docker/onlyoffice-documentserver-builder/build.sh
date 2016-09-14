2047be499e906b7929b5a903828a306449c07eb0
Alexey Golubev <alexey.golubev@onlyoffice.com>
2016-09-14 19:20:11 +0300

    Changed the build sequence

--- docker/onlyoffice-documentserver-builder/build.sh
+++ docker/onlyoffice-documentserver-builder/build.sh
@@ -2,8 +2,8 @@
 
 BUILD_DIR=/var/lib/onlyoffice
 
+cd $BUILD_DIR/core/Common/3dParty && ./make.sh
 cd $BUILD_DIR/sdkjs && make clean && make
 cd $BUILD_DIR/server && make clean && make all build-date htmlfileinternal
 cd $BUILD_DIR/document-server-integration/web/documentserver-example/nodejs && npm install
-cd $BUILD_DIR/core/Common/3dParty && ./make.sh
 cd $BUILD_DIR/package && make -e clean && make -e deb
