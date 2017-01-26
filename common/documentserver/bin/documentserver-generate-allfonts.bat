092f91c47f13e0ef5f47b387b3271ff0249d018c
Alexey.Golubev <alexey.golubev@onlyoffice.com>
2017-01-26 19:36:53 +0300

    Added decription for long action command

--- common/documentserver/bin/documentserver-generate-allfonts.bat
+++ common/documentserver/bin/documentserver-generate-allfonts.bat
@@ -1,9 +1,12 @@
 @ECHO off
-SET ONLYOFFICE_DATA_CONTAINER=false
 
+SET ONLYOFFICE_DATA_CONTAINER=false
 IF NOT "%1"=="" (
   SET ONLYOFFICE_DATA_CONTAINER=%1
 )
+
+rem Start generate AllFonts.js, font thumbnails and font_selection.bin
+ECHO | SET /p="Generating AllFonts.js, please wait..."
 
 "%~dp0\..\server\tools\AllFontsGen.exe" ^
   "%windir%\Fonts" ^
@@ -11,6 +14,9 @@ IF NOT "%1"=="" (
   "%~dp0\..\sdkjs\common\Images" ^
   "%~dp0\..\server\FileConverter\bin\font_selection.bin"
 
+ECHO Done
+
+rem Restart web-site and converter
 IF NOT "%ONLYOFFICE_DATA_CONTAINER%"=="true" (  
   net stop DsDocServiceSvc
   net start DsDocServiceSvc
--- common/documentserver/bin/documentserver-generate-allfonts.sh
+++ common/documentserver/bin/documentserver-generate-allfonts.sh
@@ -8,11 +8,15 @@ fi
 DIR="/var/www/onlyoffice/documentserver"
 
 #Start generate AllFonts.js, font thumbnails and font_selection.bin
+echo -n Generating AllFonts.js, please wait...
+
 sudo -u onlyoffice "$DIR/server/tools/AllFontsGen"\
  "/usr/share/fonts"\
  "$DIR/sdkjs/common/AllFonts.js"\
  "$DIR/sdkjs/common/Images"\
  "$DIR/server/FileConverter/bin/font_selection.bin"
+ 
+echo Done
 
 #Restart web-site and converter
 if [ "$ONLYOFFICE_DATA_CONTAINER" != "true" ]; then
