2454cfe247e931b3ec223a32698aa43cd26928c5
Alexey.Golubev <alexey.golubev@onlyoffice.com>
2017-04-27 16:55:31 +0300

    debug

--- common/documentserver/bin/documentserver-update-securelink.sh
+++ common/documentserver/bin/documentserver-update-securelink.sh
@@ -6,9 +6,9 @@ JSON="json -q -f ${DOCSERVICE_CONF}"
 
 SECRET_STRING=$(pwgen -s 20)
 
-sed "s,\(set \+\$secret_string\).*,\1 "${SECRET_STRING}"," -i ${NGINX_CONF}
+sed "s,\(set \+\$secret_string\).*,\1 "${SECRET_STRING}";," -i ${NGINX_CONF}
 
-${JSON} -I -e "storage.fs.secretString = '${SECRET_STRING}'"
+${JSON} -I -e "this.storage.fs.secretString = '${SECRET_STRING}'"
 
 sudo supervisorctl restart onlyoffice-documentserver:docservice
 sudo supervisorctl restart onlyoffice-documentserver:converter
