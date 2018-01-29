4f95184cc85258041b55c365016562776bfe6b3c
Alexey Golubev <Alexey.Golubev@onlyoffice.com>
2018-01-29 15:15:47 +0300

    Fixed custom port substitution in nginx config

--- rpm/bin/documentserver-configure.sh
+++ rpm/bin/documentserver-configure.sh
@@ -223,8 +223,8 @@ setup_nginx(){
   DS_CONF=$NGINX_CONF_DIR/onlyoffice-documentserver.conf.template
   DS_SSL_CONF=$NGINX_CONF_DIR/onlyoffice-documentserver-ssl.conf.template
   # OO_CONF=$NGINX_CONF_DIR/includes/onlyoffice-http.conf
-  sed 's/\(listen .*:\)\(80\)\(.*\)/\1'${DS_PORT}'\3/' -i $DS_CONF
-  sed 's/\(listen .*:\)\(80\)\(.*\)/\1'${DS_PORT}'\3/' -i $DS_SSL_CONF
+  sed 's/\(listen .*:\)\([0-9]\{2,5\}\)\(.*\)/\1'${DS_PORT}'\3/' -i $DS_CONF $DS_SSL_CONF
+
   # sed 's/{{DOCSERVICE_PORT}}/'${DOCSERVICE_PORT}'/' -i $OO_CONF
   # sed 's/{{SPELLCHECKER_PORT}}/'${SPELLCHECKER_PORT}'/' -i $OO_CONF
   # sed 's/{{EXAMPLE_PORT}}/'${EXAMPLE_PORT}'/' -i $OO_CONF
