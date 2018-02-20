93d76d8c3ca1a8ff8323c8eae8ed473e7a4b73c1
Alexey Golubev <alexey.golubev@onlyoffice.com>
2018-02-20 16:08:59 +0300

    Fix fonts gziping

--- common/documentserver/bin/documentserver-static-gzip.sh
+++ common/documentserver/bin/documentserver-static-gzip.sh
@@ -5,7 +5,10 @@ NGINX_ONLYOFFICE_PATH="/etc/onlyoffice/documentserver/nginx"
 
 cd ${DIR}
 # Make gziped scripts
-find ./sdkjs ./web-apps ./sdkjs-plugins ./fonts -type f \( -name *.js* -o -name *.htm* -o -name *.css -o -name *.tt* \) -exec gzip -kf9 {} \;
+find ./sdkjs ./web-apps ./sdkjs-plugins -type f \( -name *.js* -o -name *.htm* -o -name *.css -o -name *.tt* \) -exec gzip -kf9 {} \;
+
+# Make gziped fonts
+find ./fonts -type f -exec gzip -kf9 {} \;
 
 # Turn on static gzip for nginx
 sed '/expires .*;/a   gzip_static on;' \
