52d9b7b58469e9f413559539654a679faea0b5c1
Semen Bezrukov <semen.bezrukov@onlyoffice.com>
2020-09-07 14:09:45 +0300

    Move to allthemesgen

--- common/documentserver/bin/documentserver-generate-allfonts.bat
+++ common/documentserver/bin/documentserver-generate-allfonts.bat
@@ -19,28 +19,6 @@ ECHO | SET /p="Generating AllFonts.js, please wait..."
   --output-web="%~dp0\..\fonts" ^
   --use-system="true"
 
-"%~dp0\..\server\tools\allfontsgen.exe" ^
-  --input="%~dp0\..\core-fonts" ^
-  --allfonts-web="%~dp0\..\sdkjs\common\AllFonts.js" ^
-  --allfonts="%~dp0\..\server\FileConverter\bin\AllFonts.js" ^
-  --images="%~dp0\..\sdkjs\common\Images" ^
-  --selection="%~dp0\..\server\FileConverter\bin\font_selection.bin" ^
-  --output-web="%~dp0\..\fonts" ^
-  --use-system="true" ^
-  --postfix="android" ^
-  --params="280,224"
-
-"%~dp0\..\server\tools\allfontsgen.exe" ^
-  --input="%~dp0\..\core-fonts" ^
-  --allfonts-web="%~dp0\..\sdkjs\common\AllFonts.js" ^
-  --allfonts="%~dp0\..\server\FileConverter\bin\AllFonts.js" ^
-  --images="%~dp0\..\sdkjs\common\Images" ^
-  --selection="%~dp0\..\server\FileConverter\bin\font_selection.bin" ^
-  --output-web="%~dp0\..\fonts" ^
-  --use-system="true" ^
-  --postfix="ios" ^
-  --params="280,224"
-
 ECHO Done
 
 ECHO | SET /p="Generating presentation themes, please wait..."
@@ -48,6 +26,20 @@ ECHO | SET /p="Generating presentation themes, please wait..."
   --converter-dir="%~dp0\..\server\FileConverter\bin" ^
   --src="%~dp0\..\sdkjs\slide\themes" ^
   --output="%~dp0\..\sdkjs\common\Images"
+
+"%~dp0\..\server\tools\allthemesgen.exe" ^
+  --converter-dir="%~dp0\..\server\FileConverter\bin" ^
+  --src="%~dp0\..\sdkjs\slide\themes" ^
+  --output="%~dp0\..\sdkjs\common\Images" ^
+  --postfix="ios" ^
+  --params="280,224"
+
+"%~dp0\..\server\tools\allthemesgen.exe" ^
+  --converter-dir="%~dp0\..\server\FileConverter\bin" ^
+  --src="%~dp0\..\sdkjs\slide\themes" ^
+  --output="%~dp0\..\sdkjs\common\Images" ^
+  --postfix="android" ^
+  --params="280,224"
 
 ECHO Done
 
--- common/documentserver/bin/documentserver-generate-allfonts.sh.m4
+++ common/documentserver/bin/documentserver-generate-allfonts.sh.m4
@@ -20,28 +20,6 @@ echo -n Generating AllFonts.js, please wait...
   --output-web="$DIR/fonts"\
   --use-system="true"
 
-"$DIR/server/tools/allfontsgen"\
-  --input="$DIR/core-fonts"\
-  --allfonts-web="$DIR/sdkjs/common/AllFonts.js"\
-  --allfonts="$DIR/server/FileConverter/bin/AllFonts.js"\
-  --images="$DIR/sdkjs/common/Images"\
-  --selection="$DIR/server/FileConverter/bin/font_selection.bin"\
-  --output-web="$DIR/fonts"\
-  --use-system="true"\
-  --postfix="android"\
-  --params="280,224"
-
-"$DIR/server/tools/allfontsgen"\
-  --input="$DIR/core-fonts"\
-  --allfonts-web="$DIR/sdkjs/common/AllFonts.js"\
-  --allfonts="$DIR/server/FileConverter/bin/AllFonts.js"\
-  --images="$DIR/sdkjs/common/Images"\
-  --selection="$DIR/server/FileConverter/bin/font_selection.bin"\
-  --output-web="$DIR/fonts"\
-  --use-system="true"\
-  --postfix="ios"\
-  --params="280,224"
-
 chown -R ds:ds "$DIR/sdkjs"
 chown -R ds:ds "$DIR/server/FileConverter/bin"
 chown -R ds:ds "$DIR/fonts"
@@ -53,6 +31,20 @@ echo -n Generating presentation themes, please wait...
   --converter-dir="$DIR/server/FileConverter/bin"\
   --src="$DIR/sdkjs/slide/themes"\
   --output="$DIR/sdkjs/common/Images"
+
+"$DIR/server/tools/allthemesgen"\
+  --converter-dir="$DIR/server/FileConverter/bin"\
+  --src="$DIR/sdkjs/slide/themes"\
+  --output="$DIR/sdkjs/common/Images"\
+  --postfix="ios"\
+  --params="280,224"
+
+"$DIR/server/tools/allthemesgen"\
+  --converter-dir="$DIR/server/FileConverter/bin"\
+  --src="$DIR/sdkjs/slide/themes"\
+  --output="$DIR/sdkjs/common/Images"\
+  --postfix="android"\
+  --params="280,224"
 
 echo Done
 
