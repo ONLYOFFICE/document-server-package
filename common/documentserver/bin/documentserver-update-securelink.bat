e272931d33573488f52b12f9714da294a5b8d13a
eugene-kozyrev <eugene.kozyrev@onlyoffice.com>
2022-03-01 12:01:58 +0300

    Rename SECRET_STRING to SECURE_LINK_SECRET

--- common/documentserver/bin/documentserver-update-securelink.bat
+++ common/documentserver/bin/documentserver-update-securelink.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
-set SECRET_STRING=%~1
+set SECURE_LINK_SECRET=%~1
 
-if not defined SECRET_STRING (
+if not defined SECURE_LINK_SECRET (
   set "string=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
   for /L %%i in (1,1,20) do call :add
 )
@@ -14,8 +14,8 @@ set "REPLACE=replace.exe"
 set "NPM_PATH=..\npm"
 
 pushd %NPM_PATH%
-  %JSON% %DOCSERVICE_CONF% -e "this.storage.fs.secretString = '%SECRET_STRING%'"
-  %REPLACE% "(set.\$secret_string.).*;" "$1%SECRET_STRING%;" %NGINX_CONF%
+  %JSON% %DOCSERVICE_CONF% -e "this.storage.fs.secretString = '%SECURE_LINK_SECRET%'"
+  %REPLACE% "(set.\$secret_string.).*;" "$1%SECURE_LINK_SECRET%;" %NGINX_CONF%
 popd
 
 net stop DsDocServiceSvc
@@ -29,5 +29,5 @@ net start DsProxySvc
 
 :add
 set /a x=%random% %% 62 
-set SECRET_STRING=%SECRET_STRING%!string:~%x%,1!
+set SECURE_LINK_SECRET=%SECURE_LINK_SECRET%!string:~%x%,1!
 goto :eof
