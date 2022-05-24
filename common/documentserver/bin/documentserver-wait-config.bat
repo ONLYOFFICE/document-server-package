5a93b8f55bff139d9c5a836b1fa5e0de3a2f95e3
danilapog <danil.titarenko@onlyoffice.com>
2022-05-24 18:29:58 +0300

    Add timeout in maintenance script for win

--- common/documentserver/bin/documentserver-wait-config.bat
+++ common/documentserver/bin/documentserver-wait-config.bat
@@ -1,16 +1,24 @@
+
+rem This script in checking document-server status, and after check passed removing maintenance page
+rem
+
 @echo off
 
 set "MAINTENANCE_NGINX_CONF=..\nginx\conf\includes\ds-0maintenance.conf" 
 set "SERVER_URL=http://localhost:8000/healthcheck"
+set /a "timer=1"
 
 :loop
    for /f "delims=" %%a in ('
        curl -s -o nul -w "%%{http_code}" %SERVER_URL%
-   ') do set "status=%%a"
+   ') do set "status=%%a"&set "timer=%timer%+1"
 
    if not "%status%"=="200" ( 
+        if not "%timer%"=="40" (
        echo Server not reachable... trying again...
-   ) else ( 
+        ) 
+   )
+   else ( 
       echo Server reachable! Success! 
       del "%MAINTENANCE_NGINX_CONF%" 
       goto :continue
